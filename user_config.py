6d3bdbbda817125f7af1ee20c53bf779c1ea53cc
ppshiniye <119091400+ppshiniye@users.noreply.github.com>
2024-04-10 15:16:03 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,17 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-6",
+    "CCTV-8",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
